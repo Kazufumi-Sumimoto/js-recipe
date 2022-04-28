fe857b29cca78aad983ff6951d9083e17c8213a3
Kazufumi-Sumimoto <sumikona2989@au.com>
2022-04-29 02:02:00 +0900

    [□と○]の発展を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,20 @@
 const figure = document.getElementById("figure")
+const squareButton = document.getElementById("square-button")
+const circleButton = document.getElementById("circle-button")
 
+let num = 1
 figure.onclick = function () {
-  figure.classList.toggle("rounded")
+  if (num == 0) {
+    figure.classList.add("square")
+    figure.classList.remove("triangle")
+    num += 1
+  } else if (num == 1) {
+    figure.classList.add("rounded")
+    num += 1
+  } else if (num == 2) {
+    figure.classList.remove("rounded")
+    figure.classList.remove("square")
+    figure.classList.add("triangle")
+    num = 0
+  }
 }
